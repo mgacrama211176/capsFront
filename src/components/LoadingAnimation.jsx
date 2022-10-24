dd154052f82d4eaf3a65bc84e13c073f6c8c55fd
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-10-24 21:58:00 +0800

    LoadingScreen

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const LoadingAnimation = () => {
+export default function LoadingAnimation() {
   return <div>LoadingAnimation</div>;
-};
-
-export default LoadingAnimation;
+}
