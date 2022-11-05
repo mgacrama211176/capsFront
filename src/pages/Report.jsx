6477ffb1313f61aa225b7c5e539ddcdf8a482529
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-05 15:33:21 +0800

    Added reports

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,18 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import UploadPercent from "../components/UploadPercent";
+import axios from "axios";
 
-const Container = styled.div`
-  margin: 50px 0px 0px 100px;
+//MUI
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const Header = styled.h2`
+  font-family: Roboto, Arial, sans-serif;
 `;
 
-const Wrapper = styled.div``;
-
 const Report = () => {
+  const [userData, setUserData] = useState(0);
+  const [videoData, setVideoData] = useState(0);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await axios.get(`http://localhost:4000/api/users/find/All`);
+      const userContainer = users.data;
+      setUserData(userContainer);
+    };
+    const fetchVideos = async () => {
+      const videos = await axios.get(`http://localhost:4000/api/videos/trend`);
+
+      const videoContainer = videos.data;
+      setVideoData(videoContainer);
+    };
+
+    fetchUsers();
+    fetchVideos();
+  }, []);
+
   return (
     <Container>
-      <Wrapper>REPORT</Wrapper>
-      <UploadPercent />
+      <Card sx={{ width: 275, margin: 5 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Total Number of users Registered
+          </Typography>
+          <Typography variant="h5" component="div">
+            {userData.length}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ width: 275, margin: 5 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Total Number of Videos Uploaded
+          </Typography>
+          <Typography variant="h5" component="div">
+            {videoData.length}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Header>USER REPORTS</Header>
     </Container>
   );
 };
