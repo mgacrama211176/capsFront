c9834c1ae93d8b7db390bec4dbb974989138ad2a
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-12-02 12:15:35 +0800

    Update delete video bug fix

--- src/components/VideoModalDelete.jsx
+++ src/components/VideoModalDelete.jsx
@@ -100,10 +100,10 @@ const VideoModalDelete = ({ video }) => {
     try {
       //delete Videodata on Mongo
       const deleting = await axios.delete(
-        `https://capstoneback2.herokuapp.com/api/videos/${video._id}`
+        `https://filanimeback.onrender.com/api/videos/${video._id}`
       );
       const deletingAllComments = await axios.delete(
-        `https://capstoneback2.herokuapp.com/api/comments/deleteAll/${video._id}`
+        `https://filanimeback.onrender.com/api/comments/deleteAll/${video._id}`
       );
       console.log(deletingAllComments);
       console.log(deleting);
